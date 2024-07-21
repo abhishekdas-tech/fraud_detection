97b3796e01daf249b9dee35593f14fd6c97b67c3
abhishekdas <abhishekdas.tech@gmail.com>
2024-07-22 00:05:51 +0530

    Added 2 more model codes

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 print("Adding all ML related code here !!!")
+
+def model1():
+    print("Addinf code ML Model 1")
+
+def model2():
+    print("Addinf code ML Model 2")
+
+def model3():
+    print("Addinf code ML Model 3")
